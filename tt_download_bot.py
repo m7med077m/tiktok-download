0a97c2a74b3523b215de93a2be8bbdf8511483a9
m7med077m <135227322+m7med077m@users.noreply.github.com>
2023-06-06 20:10:07 +0300

    Add files via upload

---
+++ tt_download_bot.py
@@ -0,0 +1,219 @@
+import logging
+import time
+
+from aiogram import Bot, Dispatcher, executor, types
+from aiogram.contrib.fsm_storage.memory import MemoryStorage
+from aiogram.types import InlineQuery, InputTextMessageContent, InlineQueryResultArticle, InlineQueryResultVideo, InlineQueryResultAudio
+
+from re import findall
+from httpx import AsyncClient
+from hashlib import md5
+
+from tt_video import tt_videos_or_images, convert_image, divide_chunks
+from settings import languages, API_TOKEN
+
+
+storage = MemoryStorage()
+logging.basicConfig(level=logging.INFO)
+
+# Initialize bot and dispatcher
+bot = Bot(token=API_TOKEN)
+dp = Dispatcher(bot, storage=storage)
+
+
+def get_user_lang(locale):
+    user_lang = locale.language
+    if user_lang not in languages:
+        user_lang = "en"
+    return user_lang
+
+
+@dp.message_handler(commands=['start', 'help'])
+@dp.throttled(rate=2)
+async def send_welcome(message: types.Message):
+    user_lang = get_user_lang(message.from_user.locale)
+    await message.reply(languages[user_lang]["help"])
+
+
+@dp.message_handler(regexp='https://\w{1,3}\.tiktok\.com/')
+@dp.throttled(rate=3)
+async def tt_download(message: types.Message):
+    user_lang = get_user_lang(message.from_user.locale)
+
+    await message.reply(languages[user_lang]["wait"])
+    link = findall(r'\bhttps?://.*tiktok\S+', message.text)[0]
+    link = link.split("?")[0]
+
+    try:
+        response = await tt_videos_or_images(link)
+        if len(response["items"]) > 0:
+            await message.reply(f'❤️ {languages[user_lang]["likes"]}:{response["statistic"]["digg_count"]}\n💬 {languages[user_lang]["comments"]}:{response["statistic"]["comment_count"]}\n📢 {languages[user_lang]["share"]}:{response["statistic"]["share_count"]}\n👤 {languages[user_lang]["views"]}:{response["statistic"]["play_count"]}\n🗣 {languages[user_lang]["nickname"]}:{response["nickname"]}\n{response["desc"]}')
+            await message.reply_audio(response["music"], caption='@XLR_TT_BOT')
+            if response["is_video"]:
+                print("items", response["items"][0])
+                if response["large_for_tg"]:
+                    await message.reply(f'{languages[user_lang]["large_for_tg"]}:{response["items"][0]}')
+                else:
+                    await message.reply_video(response["items"][0], caption='@XLR_TT_BOT')
+            # images
+            else:
+                # metod1 download,convert to jpg, load to telegram, send MediaGroup
+                url_groups = list(divide_chunks(response["items"], 10))
+                async with AsyncClient() as client:
+                    for group in url_groups:
+                        media = types.MediaGroup()
+                        for url in group:
+                            resp = await client.get(url)
+                            img_jpeg = convert_image(resp.content, "JPEG")
+                            media.attach_photo(types.InputFile(img_jpeg))
+                        await message.reply_media_group(media=media)
+
+                # metod2 send single photos by urls as webp sticker (less internet usage)
+                # for url in response["items"]:
+                #     await message.reply_document(url, caption='@XLR_TT_BOT')
+        else:
+            await message.reply("error 0 videos or images")
+    except Exception as e:
+        print(e)
+        await message.reply(f"error: {e}")
+
+
+@dp.throttled(rate=3)
+@dp.inline_handler(regexp='https://\w{1,3}\.tiktok\.com/')
+async def inline_echo(inline_query: InlineQuery):
+
+    text = inline_query.query or 'xlr'
+
+    link = findall(r'\bhttps?://.*tiktok\S+', text)[0]
+    link = link.split("?")[0]
+
+    user_lang = get_user_lang(inline_query.from_user.locale)
+
+    try:
+        response = await tt_videos_or_images(link)
+        if len(response["items"]) > 0:
+            statisctic = f'{link}\n❤️ {languages[user_lang]["likes"]}:{response["statistic"]["digg_count"]}\n💬 {languages[user_lang]["comments"]}:{response["statistic"]["comment_count"]}\n📢 {languages[user_lang]["share"]}:{response["statistic"]["share_count"]}\n👤 {languages[user_lang]["views"]}:{response["statistic"]["play_count"]}\n🗣 {languages[user_lang]["nickname"]}:{response["nickname"]}\n{response["desc"]}'
+            audio_url = response["music"]
+
+            result_id_statistic: str = md5(
+                f"{link} statistic".encode()).hexdigest()
+            statistic = InlineQueryResultArticle(
+
+                id=result_id_statistic,
+
+                title=f'statistic: {statisctic}',
+
+                input_message_content=InputTextMessageContent(statisctic),
+
+                thumb_url="https://ak.picdn.net/shutterstock/videos/1061280574/thumb/6.jpg?ip=x480",
+
+                description=response["nickname"]
+            )
+
+            result_id_music: str = md5(
+                f"{link} music".encode()).hexdigest()
+            music = InlineQueryResultAudio(
+
+                id=result_id_music,
+
+                title='download music',
+
+                audio_url=audio_url,
+
+                performer=response["nickname"],
+
+            )
+
+            if response["is_video"]:
+                video_url = response["items"][0]
+
+                if response["large_for_tg"]:
+                    result_id_video_url: str = md5(
+                        video_url.encode()).hexdigest()
+                    video = InlineQueryResultArticle(
+
+                        id=result_id_video_url,
+
+                        title=f'video get url',
+
+                        input_message_content=InputTextMessageContent(
+                            video_url),
+                    )
+
+                else:
+                    result_id_video: str = md5(
+                        f"{link} video".encode()).hexdigest()
+
+                    video = InlineQueryResultVideo(
+
+                        id=result_id_video,
+
+                        title='download video',
+
+                        video_url=video_url,
+
+                        thumb_url=response["cover"],
+
+                        mime_type="video/mp4",
+
+                        description=response["desc"]
+                    )
+
+                await bot.answer_inline_query(inline_query.id, results=[statistic, music, video])
+
+            else:
+
+                text_url_images = '\n'.join(response["items"])
+                result_id_url_images: str = md5(
+                    text_url_images.encode()).hexdigest()
+                images = InlineQueryResultArticle(
+
+                    id=result_id_url_images,
+
+                    title=f'get url of {len(response["items"])} images',
+
+                    input_message_content=InputTextMessageContent(
+                        text_url_images),
+                )
+
+                await bot.answer_inline_query(inline_query.id, results=[statistic, music, images])
+
+        else:
+            print("0 items")
+            result_id: str = md5("0 items".encode()).hexdigest()
+            error_inline = InlineQueryResultArticle(
+
+                id=result_id,
+
+                title=f'error_0_items',
+
+                input_message_content=InputTextMessageContent('error_0_items'),
+            )
+
+            await bot.answer_inline_query(inline_query.id, results=[error_inline])
+
+    except Exception as e:
+        print("err1", e)
+        result_id: str = md5(f"err1 {e}".encode()).hexdigest()
+        error_inline = InlineQueryResultArticle(
+
+            id=result_id,
+
+            title=f'error: {e}',
+
+            input_message_content=InputTextMessageContent(f'error {e!r}'),
+        )
+
+        await bot.answer_inline_query(inline_query.id, results=[error_inline])
+
+
+@dp.message_handler()
+@dp.throttled(rate=3)
+async def echo(message: types.Message):
+    user_lang = get_user_lang(message.from_user.locale)
+
+    await message.answer(languages[user_lang]["invalid_link"])
+
+
+if __name__ == '__main__':
+    executor.start_polling(dp, skip_updates=True)
---
+++ tt_video.py
@@ -0,0 +1,90 @@
+from httpx import AsyncClient
+
+from re import findall
+from io import BytesIO
+from PIL import Image
+
+
+def divide_chunks(list, n):
+    for i in range(0, len(list), n):
+        yield list[i:i + n]
+
+
+def convert_image(image, extention):  # "JPEG"
+    byteImgIO = BytesIO()
+    byteImg = Image.open(BytesIO(image)).convert("RGB")
+    byteImg.save(byteImgIO, extention)
+    byteImgIO.seek(0)
+    return byteImgIO
+
+
+async def tt_videos_or_images(url):
+    video_id_from_url = findall('https://www.tiktok.com/@.*?/video/(\d+)', url)
+    if len(video_id_from_url) > 0:
+        video_id = video_id_from_url[0]
+    else:
+        headers1 = {
+            "User-Agent": "Mozilla/5.0 (Windows NT 6.1; Win64; x64; rv:105.0) Gecko/20100101 Firefox/105.0",
+            "Accept": "text/html, application/xhtml+xml, application/xml; q=0.9, image/avif, image/webp, */*; q=0.8"
+        }
+
+        async with AsyncClient() as client:
+            r1 = await client.get(url, headers=headers1)
+        print("r1", r1.status_code)
+        if r1.status_code == 301:
+            video_id = findall(
+                'a href="https://\w{1,3}\.tiktok\.com/(?:@.*?/video|v)/(\d+)', r1.text)[0]
+        elif r1.status_code == 403:
+            video_id = findall("video&#47;(\d+)", r1.text)[0]
+        else:
+            raise BaseException('Unknown status code', r1.status_code)
+
+    url2 = f"http://api16-normal-useast5.us.tiktokv.com/aweme/v1/aweme/detail/?aweme_id={video_id}"
+    headers2 = {
+        "User-Agent": "Mozilla/5.0 (Windows NT 6.1; Win64; x64; rv:105.0) Gecko/20100101 Firefox/105.0"}
+    async with AsyncClient() as client:
+        r2 = await client.get(url2, headers=headers2)
+    print("r2", r2.status_code)
+    resp = r2.json()
+
+    is_video = len(resp["aweme_detail"]["video"]["bit_rate"]) > 0
+    print("is_video", is_video)
+
+    nickname = resp["aweme_detail"]["author"]["nickname"]
+    desc = resp["aweme_detail"]["desc"]
+    statistic = resp["aweme_detail"]["statistics"]
+    music = resp["aweme_detail"]["music"]["play_url"]["uri"]
+    if is_video:
+        cover_url = resp["aweme_detail"]["video"]["origin_cover"]["url_list"][0]
+
+        for bit_rate in resp["aweme_detail"]["video"]["bit_rate"]:
+            height = bit_rate["play_addr"]["height"]
+            width = bit_rate["play_addr"]["width"]
+            data_size = int(bit_rate["play_addr"]["data_size"])
+
+            url_list = bit_rate["play_addr"]["url_list"]
+            quality_type = bit_rate["quality_type"]
+
+            if data_size > 19999999:
+                print("to_large_for_tg", height, "x", width, data_size /
+                      1000000, "MB", "quality_type:", quality_type)
+            else:
+                print("good_for_tg", height, "x", width, data_size /
+                      1000000, "MB", "quality_type:", quality_type)
+                videos_url = url_list
+                large_for_tg = False
+                break
+        else:
+            videos_url = resp["aweme_detail"]["video"]["bit_rate"][0]["play_addr"]["url_list"]
+            large_for_tg = True
+        return {"is_video": True, "large_for_tg": large_for_tg, "cover": cover_url, "items": videos_url, "nickname": nickname, "desc": desc, "statistic": statistic, "music": music}
+
+    else:
+        images_url = []
+        images = resp["aweme_detail"]["image_post_info"]["images"]
+        for i in images:
+            if len(i["display_image"]["url_list"]) > 0:
+                images_url.append(i["display_image"]["url_list"][0])
+            else:
+                print("err. images_url 0 len")
+        return {"is_video": False, "large_for_tg": False, "cover": None, "items": images_url, "nickname": nickname, "desc": desc, "statistic": statistic, "music": music}
